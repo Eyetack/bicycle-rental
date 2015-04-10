0db4e3bbf49b21cc1d9d9c23f8f25e4e70fbf89d
unknown <liorshahverdi@gmail.com>
2015-04-09 20:49:16 -0500

    added a fine class

---
+++ BicycleRentalCLI/Fine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BicycleRentalCLI
+{
+    class Fine
+    {
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private int borrower_id;
+        public int BorrowerID
+        {
+            get { return borrower_id; }
+            set { borrower_id = value; }
+        }
+
+        private double fine_amount;
+        public double FineAmount
+        {
+            get { return fine_amount; }
+            set { fine_amount = value; }
+        }
+
+        private string date_fine_imposed;
+        public string DateFineImposed
+        {
+            get { return date_fine_imposed; }
+            set { date_fine_imposed = value; }
+        }
+
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        private string date_status_updated;
+        public string DateStatusUpdated
+        {
+            get { return date_status_updated; }
+            set { date_status_updated = value; }
+        }
+
+        public Fine(int id, int bid, double fa, string dfi, string stat, string dsu)
+        {
+            this.ID = id;
+            this.BorrowerID = bid;
+            this.FineAmount = fa;
+            this.DateFineImposed = dfi;
+            this.Status = stat;
+            this.DateStatusUpdated = dsu;
+        }
+    }
+}
